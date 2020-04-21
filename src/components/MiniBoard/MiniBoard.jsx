6df8ed700ce593c1e7a69f8ec37f095675c7ac95
Anelvin <amejiaroque@gmail.com>
2020-04-21 22:04:58 +0200

    Sending data of the board to backend

--- src/components/MiniBoard/MiniBoard.jsx
+++ src/components/MiniBoard/MiniBoard.jsx
@@ -2,10 +2,17 @@ import React, {Component} from 'react';
 import './MiniBoard.scss';
 
 class MiniBoard extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    handleKey = () => {
+        console.log(this.props.id);
+    }
 
     render(){
         return(
-            <div className="body-mini-board">
+            <div className="body-mini-board" onClick={this.handleKey}>
                 {this.props.name}
             </div>
         )
--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -3,14 +3,27 @@ import './Dashboard.scss';
 import HeaderDashboard from '../../components/HeaderDashboard/HeaderDashboard';
 import MiniBoard from '../../components/MiniBoard/MiniBoard';
 import { connect } from 'react-redux';
+import ModalBoard from '../../components/ModalBoard/ModalBoard';
 
 class Dashboard extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            modalShow: true,
+            user:''
+        }
     }
 
-    handleParams = () => {
-        console.log(this.props.match.params.user);
+    componentDidMount(){
+        this.setState({
+            user: this.props.match.params.user
+        })
+    }
+ 
+    handleModal = () => {
+        this.setState({
+            modalShow: !this.state.modalShow
+        })
     }
 
     render(){
@@ -19,15 +32,21 @@ class Dashboard extends Component {
                 <HeaderDashboard />
                 <div className="body-dashboard">
                     <div className="sidebar">
-                        <h3>Tableros</h3>
+                        <h3 onClick={this.handleModal}>Tableros</h3>
                         <h3>Plantillas</h3>
                         <h3>Inicio</h3>
                     </div>
+                    {this.state.modalShow === true 
+                    ?
+                    <ModalBoard handleModal={this.handleModal}  user={this.state.user}/>
+                    :
+                    null 
+                    }
                     <div className="list-boards">
                         <h3>Tableros personales</h3>
                         <div>
                             {this.props.dashboars.map(board => (
-                                <MiniBoard name={board.name}/>
+                                <MiniBoard name={board.name} id={board.index}/>
                             )
                             )}
                         </div>
